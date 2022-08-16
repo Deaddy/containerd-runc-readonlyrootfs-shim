1b9282a691b8eb7c9e0c447808fed52f5891172a
Deaddy <wunderlich@uni-muenster.de>
2022-08-16 13:34:01 +0200

    meh

--- pkg/shim/util.go
+++ pkg/shim/util.go
@@ -35,7 +35,7 @@ func (s *service) getSpec(ctx context.Context) (*ocispec.Spec, error) {
 	out := &ocispec.Spec{}
 	// this is our chance to set default values for the spec
   if out.Root == nil {
-    out.Root = ocispec.Root{}
+    out.Root = &ocispec.Root{}
   }
 	out.Root.Readonly = true
 	// if any values are set, they will be overwritten by Unmarshal
